27764ff5331d0ccf5384247b061c2932ff894dcb
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-10-05 20:51:04 +0200

    Fix some calculator issues

--- public/projectsPreCourse/calculadora/calculadora.js
+++ public/projectsPreCourse/calculadora/calculadora.js
@@ -1,35 +1,30 @@
-let number1 = parseInt(prompt("Ingresa el primer numero"));
+let number1 = parseInt(prompt("Ingresa el primer numero"), 10);
 let number2;
 let operator = 0;
-while (isNaN(number1)) {
-  number1 = parseInt(prompt("Casi, ingresa el primer numero"));
+while (Number.isNaN(number1)) {
+  number1 = parseInt(prompt("Casi, ingresa el primer numero"), 10);
 }
-if (window.confirm("Quieres saber la raíz cuadrada de " + number1 + " ?")) {
+if (Window.confirm(`Quieres saber la raíz cuadrada de ${number1} ?`)) {
   operator = Math.sqrt(number1);
-  console.log("La raíz cuadrada de " + number1 + " es " + operator.toFixed(3));
+  console.log(`La raíz cuadrada de ${number1} es ${operator.toFixed(3)}`);
 } else {
   do {
-    number2 = parseInt(prompt("Ingresa el segundo numero"));
-  } while (isNaN(number2));
+    number2 = parseInt(prompt("Ingresa el segundo numero"), 10);
+  } while (Number.isNaN(number2));
   operator = number1 + number2;
   console.log(
-    "La suma de " + number1 + " + " + number2 + " es " + operator.toFixed(3)
+    `La suma de ${number1} + ${number2} es ${operator.Number.toFixed(3)}`
   );
   operator = number1 - number2;
   console.log(
-    "La resta de " + number1 + " - " + number2 + " es " + operator.toFixed(3)
+    `La resta de ${number1} - ${number2} es ${operator.Number.toFixed(3)}`
   );
   operator = number1 * number2;
   console.log(
-    "La multiplicación de " +
-      number1 +
-      " * " +
-      number2 +
-      " es " +
-      operator.toFixed(3)
+    `La multiplicación de ${number1} * ${number2} es ${operator.toFixed(3)}`
   );
   operator = number1 / number2;
   console.log(
-    "La división de " + number1 + " / " + number2 + " es " + operator.toFixed(3)
+    `La división de ${number1} / ${number2} es ${operator.toFixed(3)}`
   );
 }
